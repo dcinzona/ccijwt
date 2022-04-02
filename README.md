81dbe966046365f9384298e6c1a2a5ac5ed0a03a
Gustavo Tandeciarz <gustavo.tandeciarz@salesforce.com>
2022-04-01 22:48:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# CCI JWT Authentication POC
+
+## Setup
+
+This assumes you have a strong understanding of JWT and Connected Apps in Salesforce, created a [connected app](https://developer.salesforce.com/docs/atlas.en-us.232.0.sfdx_dev.meta/sfdx_dev/sfdx_dev_auth_connected_app.htm), and [set up the certificate](https://developer.salesforce.com/docs/atlas.en-us.232.0.sfdx_dev.meta/sfdx_dev/sfdx_dev_auth_key_and_cert.htm) within the app.
+
+After having completed the above, do the following:
+1. Copy `sample.env` to `.env`
+2. Update the parameters within `.env` (client ID, login URL, private key path, etc.)
+3. Run ```./auth.sh -u <username>```
+
+#### Help Command: `./auth.sh -h`
+
+```
+$ ./auth.sh -h
+
+Authenticate a user via JWT.
+
+Syntax: auth.sh -u [username] -a [alias] -k [cert path] -c [clientId] [-h|d|C]
+
+Options:
+
+  d     Set the org as the default SF CLI org
+  a     The org alias name for SF CLI
+  h     Print this Help
+  u     Username to authenticate
+  c     The client id of the connected app
+  C     Create the SFDX Org Connection (easy way to add the connection to your local SFDX environment)
+  k     The certificate private key to use for authentication (Default: .jwt/server.key)
+  ```
+
---
+++ auth.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
+CONNECT=0
+SET_DEFAULT=0
+_KEYPATH=.jwt/server.key
+_ALIAS=ccijwt
+
+# Local .env
+[ -f .env ] && source .env
+
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo ""
+   echo "Authenticate a user via JWT."
+   echo
+   echo "Syntax: auth.sh -u [username] -a [alias] -k [cert path] -c [clientId] [-h|d|C]"
+   echo ""
+   echo "Options:"
+   echo ""
+   echo "  d     Set the org as the default SF CLI org"
+   echo "  a     The org alias name for SF CLI"
+   echo "  h     Print this Help"
+   echo "  u     Username to authenticate"
+   echo "  c     The client id of the connected app"
+   echo "  C     Create the SFDX Org Connection (easy way to add the connection to your local SFDX environment)"
+   echo "  k     The certificate private key to use for authentication (Default: .jwt/server.key)"
+   echo
+}
+
+ConnectSFDX()
+{
+    COMMAND="sfdx force:auth:jwt:grant --clientid $_CLIENTID --username $_USERNAME --setalias $_ALIAS \
+    --jwtkeyfile $_KEYPATH "
+
+    if [[ $SET_DEFAULT -eq 1 ]];
+    then
+        COMMAND="$COMMAND --setdefaultdevhubusername "
+    fi
+    eval $COMMAND
+}
+
+PASSED_ARGS=$@
+if [[ ${#PASSED_ARGS} -ne 0 ]]
+then
+  while getopts ":c:k:u:a:Cdh" ARG; do
+    echo "\nRunning JWT CCI Test"
+    case "$ARG" in 
+        h) Help && exit 0 ;;
+        d) SET_DEFAULT=1 && echo "  Setting default org";;
+        a) _ALIAS=${OPTARG} && echo "  Set ALIAS = $_ALIAS";;
+        u) _USERNAME=${OPTARG} && echo "  Set USERNAME = $_USERNAME \n";;
+        c) _CLIENTID=${OPTARG} && echo "  Set CLIENTID = $_CLIENTID \n";;
+        k) _KEYPATH=${OPTARG} && echo "  Set KEY = $_KEYPATH \n";;
+        C) CONNECT=1;;
+        :​) echo "argument missing for $ARG" ;;
+        \?) echo "Something is wrong" ;;
+    esac
+  done
+else
+  Help
+  exit 1
+fi
+
+shift "$((OPTIND-1))"
+
+# 
+
+RunRun()
+{
+    export CUMULUSCI_ORG_ccijwt="{\"username\": \"$_USERNAME\", \"instance_url\": \"$_LOGIN_URL\"}"
+    export SFDX_CLIENT_ID=$_CLIENTID
+    export SFDX_HUB_KEY=`cat $_KEYPATH`
+    export SFDX_HUB_USERNAME=$_USERNAME
+
+    # echo "$CUMULUSCI_ORG_ccijwt"
+    # echo "$SFDX_HUB_USERNAME"
+    if [[ $CONNECT -eq 1 ]];
+    then
+        ConnectSFDX
+    fi
+    # sfdx config:set defaultdevhubusername=$_ALIAS
+    cciCommand="cci task run execute_anon --apex \"System.debug(Userinfo.getUserId());\" --org $_ALIAS"
+    echo "Executing CCI Command:\n ${YELLOW} $cciCommand ${NORMAL}\n"
+    eval $cciCommand
+}
+
+RunRun
